b638acafcffc0fdf1f76cecd5356f36f1d544b5d
Philantrope123 <nathanrenaud001@hotmail.com>
2020-10-16 12:50:22 -0400

    basic authentication logic

--- src/main/java/com/app/controller/AppController.java
+++ src/main/java/com/app/controller/AppController.java
@@ -3,10 +3,23 @@ package com.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
+
+	@GetMapping("/")
+	public String main(Model model) {
+		// main page
+		return "main";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		// login page
+		return "login";
+	}
 
 	@GetMapping("/greeting")
 	public String greet(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,18 +27,9 @@ public class AppController {
 		return "greeting";
 	}
 
-	@GetMapping("/login")
-	public String login(Model model) {
-		return "login";
-	}
-
-	@PostMapping("/login") 
-	public String auth(Model model) {
-		return "login";
-	}
-
 	@GetMapping("/error")
 	public String error(Model model) {
-
+		// error page
+		return "error";
 	}
 }
